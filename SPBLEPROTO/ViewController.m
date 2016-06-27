dbfa85d1b3ed45596607b98e75c439a4175ec83f
PocketPro <grparke@gmail.com>
2016-06-27 16:54:24 -0500

    Initial Commit

---
+++ SPBLEPROTO/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SPBLEPROTO
+//
+//  Created by Gord Parke on 6/27/16.
+//  Copyright © 2016 Gord Parke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
